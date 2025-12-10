b858ded87e20c12fe2ae0aefe1a2f2dec60f1866
Phua Qing Hao <139632696+qinghao04@users.noreply.github.com>
2025-12-10 15:19:07 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.czbb9agmslvo.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "labpassword"
+customdb = "employee"
+custombucket = "phuaqinghao-employee"
+customregion = "us-east-1"
+
